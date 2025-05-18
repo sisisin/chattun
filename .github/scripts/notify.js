1819133764df9ec40a50c25aadd54855543ba37d
sisisin <azsisisin@gmail.com>
2025-05-18 11:53:12 +0900

    setup deploy notification

---
+++ .github/scripts/notify.js
@@ -0,0 +1,40 @@
+const discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL;
+if (!discordWebhookUrl) {
+  throw new Error('DISCORD_WEBHOOK_URL is not set');
+}
+
+module.exports = async ({ github, context }) => {
+  const { owner, repo } = context.repo;
+
+  const { data: jobsData } = await github.actions.listJobsForWorkflowRun({
+    owner: owner,
+    repo: repo,
+    run_id: context.workflow_run.id,
+    per_page: 100,
+  });
+
+  const hasFailed = jobsData.jobs.some((j) => j.conclusion === 'failure');
+  if (hasFailed) {
+    await notify('failure');
+  } else {
+    await notify('success');
+  }
+};
+
+async function notify(status) {
+  const body = {
+    content: `chattun deploy ${status}`,
+    username: 'chattun bot',
+  };
+  const response = await fetch(discordWebhookUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  if (!response.ok) {
+    throw new Error(`Failed to send notification: ${response.statusText}`);
+  }
+  console.log('Notification sent successfully');
+}
