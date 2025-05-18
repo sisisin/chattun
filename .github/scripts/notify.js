6170732aeb6de2cb15113ba5e12da0088f2128d0
sisisin <azsisisin@gmail.com>
2025-05-18 12:12:53 +0900

    refactor

--- .github/scripts/notify.js
+++ .github/scripts/notify.js
@@ -4,35 +4,24 @@ if (!discordWebhookUrl) {
 }
 
 module.exports = async ({ github, context }) => {
-  const { data: jobsData } = await github.rest.actions.listJobsForWorkflowRun({
+  const { data } = await github.rest.actions.listJobsForWorkflowRun({
     owner: context.repo.owner,
     repo: context.repo.repo,
     run_id: context.payload.workflow_run.id,
     per_page: 100,
   });
 
-  const hasFailed = jobsData.jobs.some((j) => j.conclusion === 'failure');
-  if (hasFailed) {
-    await notify('failure');
-  } else {
-    await notify('success');
-  }
-};
-
-async function notify(status) {
-  const body = {
-    content: `chattun deploy ${status}`,
-    username: 'chattun bot',
-  };
+  const hasFailed = data.jobs.some((j) => j.conclusion === 'failure');
   const response = await fetch(discordWebhookUrl, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      content: `chattun deploy ${hasFailed ? 'failure' : 'success'}`,
+      username: 'chattun bot',
+    }),
   });
   if (!response.ok) {
     throw new Error(`Failed to send notification: ${response.statusText}`);
   }
   console.log('Notification sent successfully');
-}
+};
